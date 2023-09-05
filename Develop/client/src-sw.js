c28d6f0a8cd23dbb8713bc32d4a8a80feaa24fef
Mario Inzunza <marioelgallo92@gmail.com>
2023-09-05 15:37:15 -0700

    Implement asset caching

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,19 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  /\.(js|css)$/,
+  new CacheFirst({
+      cacheName: 'assets-cache',
+      plugins: [
+          new CacheableResponsePlugin({
+              statuses: [0, 200],
+          }),
+          new ExpirationPlugin({
+              maxEntries: 50, 
+              maxAgeSeconds: 30 * 24 * 60 * 60, // Cache assets for 30 days
+          }),
+      ],
+  })
+);
+
