1e955cf7223643fe735275adbd46884d6658fa0e
Mario Inzunza <marioelgallo92@gmail.com>
2023-09-05 16:02:40 -0700

    install js

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,10 +2,31 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default browser prompt
+  event.preventDefault();
+  
+  const deferredPrompt = event;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+  butInstall.style.display = 'block';
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  // TODO: Implement logic to handle the installation when the button is clicked
+  butInstall.addEventListener('click', async () => {
+    deferredPrompt.prompt();
+
+    const choiceResult = await deferredPrompt.userChoice;
+
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the PWA installation');
+    } else {
+      console.log('User declined the PWA installation');
+    }
+
+    butInstall.style.display = 'none';
+  });
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('App installed successfully');
+});
+
